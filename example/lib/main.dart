cd507f399259aff7a0d1c0b4c04275ca1c7a9603
Kabo <kaboc.app@gmail.com>
2021-03-02 01:40:57 +0900

    Update example to include SingleGlitter

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:glitters/glitters.dart';
+import 'package:glitters/single_glitter.dart';
 
 void main() => runApp(const App());
 
@@ -19,45 +20,68 @@ class App extends StatelessWidget {
           backgroundColor: Colors.grey.shade700,
         ),
         body: SafeArea(
-          child: Center(
-            child: FractionallySizedBox(
-              widthFactor: 0.6,
-              heightFactor: 0.6,
-              child: ColoredBox(
-                color: Colors.blueGrey.shade700,
-                child: Stack(
-                  children: const <Widget>[
-                    Glitters(
-                      minSize: 8.0,
-                      maxSize: 20.0,
-                      interval: Duration.zero,
-                      maxOpacity: 0.7,
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Glitters'),
+                const SizedBox(height: 4.0),
+                Expanded(
+                  flex: 2,
+                  child: ColoredBox(
+                    color: Colors.blueGrey.shade700,
+                    child: Stack(
+                      children: const <Widget>[
+                        Glitters(
+                          minSize: 8.0,
+                          maxSize: 20.0,
+                          interval: Duration.zero,
+                          maxOpacity: 0.7,
+                        ),
+                        Glitters(
+                          minSize: 10.0,
+                          maxSize: 25.0,
+                          interval: Duration(milliseconds: 20),
+                          color: Colors.lime,
+                        ),
+                        Glitters(
+                          minSize: 10.0,
+                          maxSize: 25.0,
+                          duration: Duration(milliseconds: 200),
+                          inDuration: Duration(milliseconds: 500),
+                          outDuration: Duration(milliseconds: 500),
+                          interval: Duration(milliseconds: 30),
+                          color: Colors.white,
+                          maxOpacity: 0.7,
+                        ),
+                        Glitters(
+                          minSize: 14.0,
+                          maxSize: 30.0,
+                          interval: Duration(milliseconds: 40),
+                          color: Colors.orange,
+                        ),
+                      ],
                     ),
-                    Glitters(
-                      minSize: 10.0,
-                      maxSize: 25.0,
-                      interval: Duration(milliseconds: 20),
-                      color: Colors.lime,
-                    ),
-                    Glitters(
-                      minSize: 10.0,
-                      maxSize: 25.0,
-                      duration: Duration(milliseconds: 200),
-                      inDuration: Duration(milliseconds: 500),
-                      outDuration: Duration(milliseconds: 500),
-                      interval: Duration(milliseconds: 30),
-                      color: Colors.white,
-                      maxOpacity: 0.7,
-                    ),
-                    Glitters(
-                      minSize: 14.0,
-                      maxSize: 30.0,
-                      interval: Duration(milliseconds: 40),
-                      color: Colors.orange,
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+                const SizedBox(height: 36.0),
+                const Text('SingleGlitter'),
+                const SizedBox(height: 4.0),
+                Expanded(
+                  child: ColoredBox(
+                    color: Colors.blueGrey.shade700,
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        return SingleGlitter(
+                          size: constraints.biggest.shortestSide,
+                          color: Colors.amber,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
